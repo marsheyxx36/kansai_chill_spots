612dfdb3085be4b31013d0d4f836259d3d19777c
marsheyxx36 <masshun36@gmail.com>
2021-02-26 00:51:54 +0900

    修正

--- app/javascript/googlemap.js
+++ app/javascript/googlemap.js
@@ -1,31 +1,62 @@
-
-
-let map 
+window.addEventListener('load', function(){
+let map
 let geocoder
-let position = document.getElementById('position_show')
-let position2 = document.getElementById('position_show2')
 // 下の1行を追加 
 const display = document.getElementById('display')
 
-function initMap(){
+var url = location.href ;
+if(document.URL.match("new")) {
+  function initMap(){
     geocoder = new google.maps.Geocoder()
   
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat:35.68123620000001, lng:139.7671248},
+      center: {lat:35.6809591 , lng:139.7673068},
       zoom: 16,
     });
   
     marker = new google.maps.Marker({
-      position:{lat:35.68123620000001, lng:139.7671248},
+      position:{lat:35.6809591 , lng:139.7673068},
       map: map
     });
-    var html = `<script src="https://maps.googleapis.com/maps/api/js?key=<%=ENV['GOOGLE_MAP_API_KEY']%>&callback=initMap" async defer></script>`
-
-    document.body.appendChild(html);
   }
+  initMap();
 
-function ShowInitMap(){
-    if(window.location.href.match(/\/posts\/\d+\/show/)){
+  function codeAddress(){
+    let inputAddress = document.getElementById('address').value;
+  
+    geocoder.geocode( { 'address': inputAddress}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+        // 下の1行を追加 
+        display.textContent = "検索結果：" + results[ 0 ].geometry.location
+        let location = results[ 0 ].geometry.location
+        var lat = location.lat();
+        var lng = location.lng();    
+        document.getElementById( "latitude" ).value = lat
+         document.getElementById( "longitude" ).value = lng
+    
+      } else {
+        alert('該当する結果がありませんでした：' + status);
+      }
+    });
+    
+  }
+  var button = document.getElementById("map_search")
+  button.addEventListener('click', function() {
+    codeAddress();
+  });
+  
+
+}else{
+
+  let position = document.getElementById('position_show')
+  let position2 = document.getElementById('position_show2')
+  console.log(Number(position.innerHTML))
+  function initMap(){
     geocoder = new google.maps.Geocoder()
   
     map = new google.maps.Map(document.getElementById('map'), {
@@ -37,33 +68,9 @@ function ShowInitMap(){
       position:{lat:Number(position.innerHTML), lng:Number(position2.innerHTML)},
       map: map
     });
-
-    var html = `<script src="https://maps.googleapis.com/maps/api/js?key=<%=ENV['GOOGLE_MAP_API_KEY']%>&callback=initMap" async defer></script>`
-
-    document.body.appendChild(html);
-}
   }
-
-function codeAddress(){
-  let inputAddress = document.getElementById('address').value;
-
-  geocoder.geocode( { 'address': inputAddress}, function(results, status) {
-    if (status == 'OK') {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
-      // 下の1行を追加 
-      display.textContent = "検索結果：" + results[ 0 ].geometry.location
-      let location = results[ 0 ].geometry.location
-      var lat = location.lat();
-      var lng = location.lng();    
-      document.getElementById( "latitude" ).value = lat
-       document.getElementById( "longitude" ).value = lng
-  
-    } else {
-      alert('該当する結果がありませんでした：' + status);
-    }
-  });   
+  initMap();
 }
+
+
+});
