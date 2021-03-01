2d38843c0c071287cbdf4f6bcacc7f9eb7733c99
marsheyxx36 <masshun36@gmail.com>
2021-03-01 19:47:23 +0900

    編集機能・削除機能の実装

--- app/javascript/googlemap.js
+++ app/javascript/googlemap.js
@@ -26,6 +26,36 @@ if(url2.match(/\d{2,4}/)) {
   }
   initMap();
   
+  function codeAddress(){
+    let inputAddress = document.getElementById('address').value;
+  
+    geocoder.geocode( { 'address': inputAddress}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+        // 下の1行を追加 
+        display.textContent = "検索結果：" + results[ 0 ].geometry.location
+        let location = results[ 0 ].geometry.location
+        var lat = location.lat();
+        var lng = location.lng();    
+        document.getElementById( "latitude" ).value = lat
+         document.getElementById( "longitude" ).value = lng
+    
+      } else {
+        alert('該当する結果がありませんでした：' + status);
+      }
+    });
+    
+  }
+  var button = document.getElementById("map_search")
+  button.addEventListener('click', function() {
+    codeAddress();
+  });
+
+
 
 }else{
   function initMap(){
