0119f04daaad98504714aa5f9324622873e3fc47
marsheyxx36 <masshun36@gmail.com>
2021-03-06 11:42:38 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
-
   before_action :authenticate_user!, except: [:show, :index]
-
+  
   def index
     @posts = Post.all
     @parents = Area.all.limit(6)
