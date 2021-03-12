447369900cdd116b3e09d69cedd7e2746ea305a2
marsheyxx36 <masshun36@gmail.com>
2021-03-13 00:11:48 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,13 @@ class User < ApplicationRecord
   def liked_by?(post_id)
     likes.where(post_id: post_id).exists?
   end
+
+  devise :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
