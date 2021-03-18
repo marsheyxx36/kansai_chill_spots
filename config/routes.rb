e62389f50e101fe1807cff11d2a5d349222d5cde
marsheyxx36 <masshun36@gmail.com>
2021-03-18 15:33:12 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,6 @@ Rails.application.routes.draw do
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
    
+  resources :users, :only => [:show]
 
 end
