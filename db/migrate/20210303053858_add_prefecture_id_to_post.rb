24094aa07cee86da56eec2c10a046b666245efcc
marsheyxx36 <masshun36@gmail.com>
2021-04-04 20:16:35 +0900

    Update 20210303053858_add_prefecture_id_to_post.rb

--- db/migrate/20210303053858_add_prefecture_id_to_post.rb
+++ db/migrate/20210303053858_add_prefecture_id_to_post.rb
@@ -1,5 +1,5 @@
 class AddPrefectureIdToPost < ActiveRecord::Migration[6.0]
   def change
-    add_reference :posts, :prefecture, null: false, foreign_key: true
+    add_column :posts, :prefecture, :integer ,null: false
   end
 end
