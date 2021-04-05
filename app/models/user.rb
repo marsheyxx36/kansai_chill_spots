56d813f9dcd9c003a4b41818ed48345a6acae77f
marsheyxx36 <masshun36@gmail.com>
2021-04-05 17:27:43 +0900

    レスポンシブ対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, presence: true
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
